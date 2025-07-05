d9dd7774d568dcb855da296f9a2cad59df464fe3
brenddonanjos <brenddon.dev@gmail.com>
2025-07-05 16:56:12 -0300

    [feature]: High contrast mode

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 new window.VLibras.Widget('https://vlibras.gov.br/app');
+
+document.addEventListener('DOMContentLoaded', function() {
+  const highContrastButton = document.getElementById('highContrast');
+  const body = document.body;
+
+  highContrastButton.addEventListener('click', function() {
+    body.classList.toggle('high-contrast');
+  });
+});
