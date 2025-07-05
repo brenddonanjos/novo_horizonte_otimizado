59bdb612a93b8bb4aa85a20dd44a6111d246eab8
Luiz Eduardo Amorim <eduardogbi10@gmail.com>
2025-07-05 17:21:43 -0300

    [feature]: Add logic to increase and decrease font-size

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-new window.VLibras.Widget('https://vlibras.gov.br/app');
+const app = (() => {
+  const ativarVLibras = () => {
+    new window.VLibras.Widget('https://vlibras.gov.br/app');
+  }
 
-document.addEventListener('DOMContentLoaded', function() {
-  const highContrastButton = document.getElementById('highContrast');
-  const body = document.body;
+  const toggleAutoContraste = () => {
+    const highContrastButton = document.getElementById('highContrast');
+    const body = document.body;
 
-  highContrastButton.addEventListener('click', function() {
-    body.classList.toggle('high-contrast');
-  });
-});
+    highContrastButton.addEventListener('click', () => body.classList.toggle('high-contrast'));
+  }
+
+  const aumentarFonte = () => {
+    const increaseFontButton = document.getElementById('increaseFont');
+    const rootElement = document.querySelector("html");
+
+    increaseFontButton.addEventListener('click', () => {
+      const computedFontSize = window.getComputedStyle(rootElement).fontSize.slice(0, -2);
+      const currentFontSize = Number(computedFontSize);
+      rootElement.style.fontSize = `${currentFontSize + 1}px`;
+    });
+  }
+
+  const diminuirFonte = () => {
+    const decreaseFontButton = document.getElementById('decreaseFont');
+    const rootElement = document.querySelector("html");
+
+    decreaseFontButton.addEventListener('click', () => {
+      const computedFontSize = window.getComputedStyle(rootElement).fontSize.slice(0, -2);
+      const currentFontSize = Number(computedFontSize);
+      rootElement.style.fontSize = `${currentFontSize - 1}px`;
+    });
+  }
+
+  return {
+    init() {
+      toggleAutoContraste();
+      ativarVLibras();
+      aumentarFonte();
+      diminuirFonte();
+    }
+  }
+})();
+
+document.addEventListener("DOMContentLoaded", app.init);
